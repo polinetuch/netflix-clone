ba404740fd2667c1b093994b553b9a6ef0ceba8f
Polinet <uchpolinet@gmail.com>
2020-08-31 18:49:41 +1000

    passed fetch url from request file to apps

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Row from './Rows/Row';
+import request from './Request/request';
 
 function App() {
   return (
     <div className="App">
       <h1>Cloning Netflix...</h1>
-      <Row title="Netflix Original"/>
-      <Row title="Trending Now"/>
-      <Row title="Top Rated"/>
-      <Row title="Action Movies"/>
-      <Row title="Comedy"/>
-      <Row title="Horror Movies"/>
-      <Row title="Romance"/>
-      <Row title="Documentaries"/>
+      <Row title="Netflix Original" fetchUrl={request.fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchUrl={request.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={request.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={request.fetchActionMovies}/>
+      <Row title="Comedy" fetchUrl={request.fetchComdeyMovies}/>
+      <Row title="Horror Movies" fetchUrl={request.fetchComdeyMovies}/>
+      <Row title="Romance" fetchUrl={request.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={request.fetchDocumentaries}/>
     </div>
   );
 }
