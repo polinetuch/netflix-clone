2049f3c4015bd8ef497709e03686a3607903ed8e
Polinet <uchpolinet@gmail.com>
2020-08-31 21:35:32 +1000

    edited param query error

--- src/Request/request.js
+++ src/Request/request.js
@@ -2,13 +2,13 @@ const api_key = "00dc8509ab90db912ef968f68b9baec3";
 
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${api_key}&language=enUS`,
-    fetchNetflixOriginals: `/discover/tv?api_key=${api_key}&width_networks=213`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${api_key}&with_networks=213`,
     fetchTopRated: `/movie/top_rated?api_key=${api_key}&language=en-US`,
-    fetchActionMovies: `/discover/movie?api_key=${api_key}&width_genres=28`,
-    fetchComedyMovies: `/discover/movie?api_key=${api_key}&width_genres=35`,
-    fetchHorrorMovies: `/discover/movie?api_key=${api_key}width_genres=27`,
-    fetchRomanceMovies: `/discover/movie?api_key=${api_key}width_genres=10749`,
-    fetchDocumentaries: `/discover/movie?api_key=${api_key}width_genres=99`,
+    fetchActionMovies: `/discover/movie?api_key=${api_key}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${api_key}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${api_key}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${api_key}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${api_key}&with_genres=99`,
 };
 
 export default requests;
