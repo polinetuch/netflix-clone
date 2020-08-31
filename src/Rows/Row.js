c95925a7d901d821cd7e914bbca5c1cee39cb7bd
Polinet <uchpolinet@gmail.com>
2020-08-31 21:24:54 +1000

    fetched movie data & displayed image + name

--- src/Rows/Row.js
+++ src/Rows/Row.js
@@ -1,11 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from '../Axios/axios';
+import './Row.css';
 
-const Row = ({title}) => {
+const base_url = "https://image.tmdb.org/t/p/original/"
+
+const Row = ({title, fetchUrl}) => {
     const [movies, setMovies] = useState([]);
-    
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+        }
+        fetchData();
+    }, [fetchUrl]);
+
     return (
-        <div>
+        <div className="row">
             <h2>{title}</h2>
+
+            <div className="row__posters-container">
+            {
+                movies.map((movie, index) => {
+                    return <img 
+                            key={index} src={base_url + movie.poster_path} 
+                            alt={movie.name}
+                            className="row__poster"
+                            />
+                })
+            }
+            </div>
         </div>
     )
 }
